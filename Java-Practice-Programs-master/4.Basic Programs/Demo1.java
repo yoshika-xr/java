7c462edbe75b624187e9ecff362df03518a0262d
yoshika-xr <yoshikajune@gmail.com>
2026-02-07 22:07:09 +0530

    day3

--- Java-Practice-Programs-master/4.Basic Programs/Demo1.java
+++ Java-Practice-Programs-master/4.Basic Programs/Demo1.java
@@ -4,12 +4,12 @@ public class Demo1 {
     }
     public static void find_count(int n)
     {
-        /* int count=0;
-        for(int i=n;i>0;i=i/10)
-        {
-            count++;
-        }
-        System.out.println(count); */
+        //  int count=0;
+        // for(int i=n;i>0;i=i/10)
+        // {
+        //     count++;
+        // }
+        // System.out.println(count); 
         //! By Taking Logarithm
         
         System.out.println((int)Math.log10(n)+1);
