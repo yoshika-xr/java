157abea92ce4c33c1d858547b8b24f6d37237ba0
yoshika-xr <yoshikajune@gmail.com>
2026-02-16 22:44:54 +0530

    day5

--- day3/java3.java
+++ day3/java3.java
@@ -7,22 +7,22 @@ class java3 {
         Scanner sc = new Scanner(System.in);
         //found count of the digit//
         {
-            //    int a = sc.nextInt(); 
-            //     int count = 0;
-            //     for (int i = a; i > 0; i = i / 10) {
-            //         count++;
-            //     }
-            //     System.out.println(count);
+               int a = sc.nextInt(); 
+                int count = 0;
+                for (int i = a; i > 0; i = i / 10) {
+                    count++;
+                }
+                System.out.println(count);
         }
         //found divisor of given number//
         {
-            // int num = 60;
-            // for (int i = 2; i <= num/2; i++) {
-            //     if (num % i == 0) {
+            int num = 60;
+            for (int i = 2; i <= num/2; i++) {
+                if (num % i == 0) {
 
-            //         System.out.print(i+" ");
-            //     }
-            // }
+                    System.out.print(i+" ");
+                }
+            }
         }
         
     }
