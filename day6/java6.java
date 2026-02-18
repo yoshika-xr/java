e23c5494d5db068cf2bd98d1a94fe3c3b9bf5ed1
yoshika-xr <yoshikajune@gmail.com>
2026-02-18 08:10:39 +0530

    day6

--- day6/java6.java
+++ day6/java6.java
@@ -4,14 +4,22 @@ class java6 {
     public static void main(String[] args) {
         //revise the given number//
         {
-            int num = 8865765;
-            int sum = 0;
-            while (num != 0) {
-                int n = num % 10;
-                sum = sum * 10 + n;
-                num /= 10;
+            // int num = 8865765;
+            // int sum = 0;
+            // while (num != 0) {
+            //     int n = num % 10;
+            //     sum = sum * 10 + n;
+            //     num /= 10;
+            // }
+            // System.out.println(sum);
+        }
+        {
+            int num = 123;
+            int count = 0;
+            for (int i = num; i > 0; i/= 10) {
+                count++;
             }
-            System.out.println(sum);
+            System.out.println(count);
         }
     }
 }
