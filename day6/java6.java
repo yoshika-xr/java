54cabd6525dc5367d0377872b92906a1312c943c
yoshika-xr <yoshikajune@gmail.com>
2026-02-18 18:46:14 +0530

    day6

--- day6/java6.java
+++ day6/java6.java
@@ -4,22 +4,54 @@ class java6 {
     public static void main(String[] args) {
         //revise the given number//
         {
-            // int num = 8865765;
-            // int sum = 0;
-            // while (num != 0) {
-            //     int n = num % 10;
-            //     sum = sum * 10 + n;
-            //     num /= 10;
-            // }
-            // System.out.println(sum);
+            int num = 1534236469;
+            int sum = 0;
+            while (num != 0) {
+                int n = num % 10;
+                if (sum > Integer.MAX_VALUE / 10
+                        || (sum == Integer.MAX_VALUE / 10 && num > 7)) {
+                    System.out.println(0);
+                }
+
+                if (sum < Integer.MIN_VALUE / 10
+                        || (sum == Integer.MIN_VALUE / 10 && num < -8)) {
+                    System.out.println(0);
+                }
+                sum = sum * 10 + n;
+                num /= 10;
+            }
+            System.out.println(sum);
         }
         {
-            int num = 123;
-            int count = 0;
-            for (int i = num; i > 0; i/= 10) {
-                count++;
+          
+        }
+        {
+            // int num = 123;
+            // int count = 0;
+            // for (int i = num; i > 0; i/= 10) {
+            //     count++;
+            // }
+            // System.out.println(count);
+        }
+        {
+            // Class containing the method to find string length
+            class Solution {
+                // Function to return length of a string
+
+                public int findLength(String s) {
+                    // Return length using built-in function
+                    return s.length();
+                }
             }
-            System.out.println(count);
+            // Driver class
+
+            // Create object of Solution class
+            Solution obj = new Solution();
+            // Input string
+            String s = "Hello World";
+            // Call function and print result
+            System.out.println(obj.findLength(s));
         }
     }
+
 }
