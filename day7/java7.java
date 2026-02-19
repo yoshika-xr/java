9c60cb0532aa3a2dd5d95052361466d9a1063e86
yoshika-xr <yoshikajune@gmail.com>
2026-02-19 19:02:00 +0530

    day7

---
+++ day7/java7.java
@@ -0,0 +1,65 @@
+class java6{
+    public static void main(String[]arg){
+          {//1
+
+            // for(int i=1;i<=5;i++){
+            //     for(int j=1;j<=5;j++){
+            //         System.out.print("*");
+            //     }
+            //     System.out.println();
+            // }
+        }
+        {//2
+            // for (int i = 1; i <= 5; i++) {
+            //     for (int j = 1; j <= i; j++) {
+            //         System.out.print("*");
+            //     }
+            //     System.out.println();
+            // }
+        }
+        {//3
+            //  for (int i = 1; i <= 5; i++) {
+            //     for (int j = 1; j <= i; j++) {
+            //         System.out.print(j);
+            //     }
+            //     System.out.println();
+            // }
+        }
+    }
+}
+
+
+
+
+
+
+
+// ## 🔢 Pattern List
+
+// ### 1. Solid Square
+
+// ```
+// *****
+// *****
+// *****
+// *****
+// *****
+// ```
+
+// ### 2. Right-Angled Triangle (Increasing)
+
+// ```
+// *
+// **
+// ***
+// ****
+// *****
+// ```
+
+//3.numbers triangle
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
