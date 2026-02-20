06bebf76acf505924852747e7c85a3d6e6f39b89
yoshika-xr <yoshikajune@gmail.com>
2026-02-20 20:07:33 +0530

    day8

--- day6/java6.java
+++ day6/java6.java
@@ -49,20 +49,20 @@ class java6 {
             // }
         }
         {
-            // for (int i = 1; i <= 5; i++) {
-            //     for (int j = 1; j <= i; j++) {
-            //         System.out.print("*");
-            //     }
-            //     System.out.println();
-            // }
-        }
-        {
-             for (int i = 1; i <= 5; i++) {
-                for (int j = 1; j <= i; j++) {
+            for (int i = 1; i <= 5; i++) {
+                for (int j = 1; j <=5-i+1; j++) {
                     System.out.print(j);
                 }
                 System.out.println();
             }
+        }
+        {
+            //  for (int i = 1; i <= 5; i++) {
+            //     for (int j = 1; j <= i; j++) {
+            //         System.out.print(j);
+            //     }
+            //     System.out.println();
+            // }
         }
     }
 
--- day8/java8.java
+++ day8/java8.java
@@ -1,11 +1,9 @@
-
 class java8 {
-
     public static void main(String[] args) {
         {//1
         int n=5;
             for (int i = 1; i <= n; i++) {
-                for (int j = 1; j >= i; j++) {
+                for (int j = i; j <= n; j++) {
                     System.out.print(j);
                 }
                 System.out.println();
