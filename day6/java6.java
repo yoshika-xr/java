e6da40a6178c1c173022ae08be9ee5782874bdf3
yoshika-xr <yoshikajune@gmail.com>
2026-03-03 07:36:34 +0530

    some changes

--- day6/java6.java
+++ day6/java6.java
@@ -2,25 +2,16 @@
 class java6 {
 
     public static void main(String[] args) {
-        //revise the given number//
+        //reverse the given number//
         {
-            // int num = 1534236469;
-            // int sum = 0;
-            // while (num != 0) {
-            //     int n = num % 10;
-            //     if (sum > Integer.MAX_VALUE / 10
-            //             || (sum == Integer.MAX_VALUE / 10 && num > 7)) {
-            //         System.out.println(0);
-            //     }
-
-            //     if (sum < Integer.MIN_VALUE / 10
-            //             || (sum == Integer.MIN_VALUE / 10 && num < -8)) {
-            //         System.out.println(0);
-            //     }
-            //     sum = sum * 10 + n;
-            //     num /= 10;
-            // }
-            // System.out.println(sum);
+            int num=1234;
+            int sum=0;
+            while(num !=0){
+                int n=num%10;
+                sum=sum*10+n;
+                num=num/10;
+            }   
+            System.out.println(sum);
         }
         {
             // for (int i = 0; i < 3; i++) {
@@ -49,12 +40,12 @@ class java6 {
             // }
         }
         {
-            for (int i = 1; i <= 5; i++) {
-                for (int j = 1; j <=5-i+1; j++) {
-                    System.out.print(j);
-                }
-                System.out.println();
-            }
+            // for (int i = 1; i <= 5; i++) {
+            //     for (int j = 1; j <=5-i+1; j++) {
+            //         System.out.print(j);
+            //     }
+            //     System.out.println();
+            // }
         }
         {
             //  for (int i = 1; i <= 5; i++) {
