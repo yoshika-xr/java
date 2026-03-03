5afeecc2a97a72740da68261827f8c1692cbd171
yoshika-xr <yoshikajune@gmail.com>
2026-03-03 07:43:16 +0530

    add topic and output

--- day6/java6.java
+++ day6/java6.java
@@ -4,15 +4,16 @@ class java6 {
     public static void main(String[] args) {
         //reverse the given number//
         {
-            int num=1234;
-            int sum=0;
-            while(num !=0){
-                int n=num%10;
-                sum=sum*10+n;
-                num=num/10;
-            }   
-            System.out.println(sum);
+            // int num=1234;
+            // int sum=0;
+            // while(num !=0){
+            //     int n=num%10;
+            //     sum=sum*10+n;
+            //     num=num/10;
+            // }   
+            // System.out.println(sum);
         }
+        //how  the loop works//
         {
             // for (int i = 0; i < 3; i++) {
             //     for (int j = 0; j < 3; j++) {
@@ -22,6 +23,7 @@ class java6 {
             //     }
             // }
         }
+        //count of the digit//
         {
             // int num = 123;
             // int count = 0;
@@ -38,22 +40,40 @@ class java6 {
             //     }
             //     System.out.println();
             // }
+
+            // *****
+            // *****
+            // *****
+            // *****
+            // *****
         }
         {
+            // int n = 5;
             // for (int i = 1; i <= 5; i++) {
-            //     for (int j = 1; j <=5-i+1; j++) {
+            //     for (int j = 1; j <= n - i + 1; j++) {
             //         System.out.print(j);
             //     }
             //     System.out.println();
             // }
+            // 12345
+            // 1234        
+            // 123
+            // 12
+            // 1
         }
         {
-            //  for (int i = 1; i <= 5; i++) {
-            //     for (int j = 1; j <= i; j++) {
-            //         System.out.print(j);
-            //     }
-            //     System.out.println();
-            // }
+             for (int i = 1; i <= 5; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+
+            // 1
+            // 12
+            // 123
+            // 1234
+            // 12345
         }
     }
 
