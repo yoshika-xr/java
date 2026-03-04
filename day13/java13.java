39530570ca9d533c77bfbafd0fed32912badca2f
yoshika-xr <yoshikajune@gmail.com>
2026-03-04 07:46:50 +0530

    day13

--- day13/java13.java
+++ day13/java13.java
@@ -1,23 +1,26 @@
 
+import java.util.Scanner;
+
 class java13 {
 
-    static void reverseNumber(int num) {
+    static int reverseNumber(int num) {
         int sum = 0;
         while (num != 0) {
             int n = num % 10;
             sum = sum * 10 + n;
             num = num / 10;
         }
-        System.out.println(sum);
+        return (sum);
 
     }
 
     public static void main(String[] args) {
-        int num = 1234;
-        int obj=reverseNumber(num);
-        if(num==obj){
-            System.out.println(".()");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num == reverseNumber(num)) {
+            System.out.println("palindrome");
+        } else {
+            System.out.println("not palindrome");
         }
-        System.out.println(num);
     }
 }
