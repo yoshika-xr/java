427c949153c86ddf5e526b2fd8652abcbe4c890f
yoshika-xr <yoshikajune@gmail.com>
2026-03-06 07:58:07 +0530

    changes

--- day5/java5.java
+++ day5/java5.java
@@ -14,5 +14,16 @@ class java5 {
             }
             System.out.println(sum);
         }
+         //reverse the given number//
+        {
+            // int num=1234;
+            // int sum=0;
+            // while(num !=0){
+            //     int n=num%10;
+            //     sum=sum*10+n;
+            //     num=num/10;
+            // }   
+            // System.out.println(sum);
+        }
     }
 }
