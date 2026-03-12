bb1f226b5483cf2c840bcaa6791942e4b70ef9fb
yoshika-xr <yoshikajune@gmail.com>
2026-03-12 22:36:55 +0530

    add

---
+++ basic-problems/no8.java
@@ -0,0 +1,11 @@
+
+class no8 {
+
+    public static void main(String args[]) {
+        int[] numbers = {10, 20, 30, 40};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
