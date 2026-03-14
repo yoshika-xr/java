3b84d946f8018ac0cf0ddbd39857a565b4fa9fe4
yoshika-xr <yoshikajune@gmail.com>
2026-03-14 22:15:09 +0530

    no8

--- basic-problems/no8.java
+++ basic-problems/no8.java
@@ -1,11 +1,15 @@
 
+import java.util.Arrays;
+
+
 class no8 {
 
     public static void main(String args[]) {
         int[] numbers = {10, 20, 30, 40};
+        System.out.println(Arrays.toString(numbers));
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+        // for (int i = 0; i < numbers.length; i++) {
+        //     System.out.print(Arrays.toString(numbers[i]));
+        // }
     }
 }
