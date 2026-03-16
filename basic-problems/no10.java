8c5a5650999f6bba2ab9f1badbdd41b60d5d2c83
yoshika-xr <yoshikajune@gmail.com>
2026-03-16 18:59:48 +0530

    no10

---
+++ basic-problems/no10.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+class no10 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int o = sc.nextInt();
+        int t = sc.nextInt();
+        int th = sc.nextInt();
+        int fo = sc.nextInt();
+        int fiv = sc.nextInt();
+        int[] ord={o,t,th,fo,fiv};
+        System.out.println(o+t+th+fo+fiv);
+    }
+}
