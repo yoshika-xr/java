05097a8b6abedbc8fe5ee3c73b1de4b450058b9e
yoshika-xr <yoshikajune@gmail.com>
2026-03-18 08:07:20 +0530

    changeeeno10

--- basic-problems/no10.java
+++ basic-problems/no10.java
@@ -1,47 +1,24 @@
 
-        import java.util.Arrays;
+class no10 {
 
-class Solution {
-    /* Function to find all 
-    divisors of n */
-    public int[] divisors(int n) {
-        
-        // Initial size of the array is set to n
-        int[] temp = new int[n];
+    public int[] divisor(int n) {
+        int[] ans = new int[n];
         int count = 0;
-        
-        // Iterate from 1 to n
         for (int i = 1; i <= n; i++) {
-            
-            // If a divisor is found
             if (n % i == 0) {
-                // Add it to the array
-                temp[count++] = i;
+                ans[count++] = i;
             }
         }
-        
-        /* Copy the divisors to an 
-        array of the exact size */
-        int[] ans = Arrays.copyOf(temp, count);
-        
-        // Return the result
-        return ans;
+        return (ans);
     }
 
     public static void main(String[] args) {
-        int n = 6;
-        
-        /* Creating an instance of 
-        Solution class */
-        Solution sol = new Solution();
-        
-        /* Function call to find 
-        all divisors of n */
-        int[] ans = sol.divisors(n);
-        
-        System.out.print("The divisors of " + n + " are: ");
-        for (int i = 0; i < ans.length; i++) {
-            System.out.print(ans[i] + " ");
+        no10 obj = new no10();
+        int[] resu=obj.divisor(10);
+        for (int i = 0; i < resu.length; i++) {
+            if (resu[i] != 0) {
+                System.out.print(resu[i] + " ");
+            }
         }
     }
 }
