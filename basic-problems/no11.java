954ac17bc00349ea6fa7dc2ba832961cb9bac6dd
yoshika-xr <yoshikajune@gmail.com>
2026-03-21 23:20:44 +0530

    no11

---
+++ basic-problems/no11.java
@@ -0,0 +1,16 @@
+
+class no11 {
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 6;
+        int gcd = 1;
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD is: " + gcd);
+    }
+}
