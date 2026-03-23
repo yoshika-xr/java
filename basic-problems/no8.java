2a5031b4b8b6a7cd5a1c3cb98bbbd3d4a4f1628a
yoshika-xr <yoshikajune@gmail.com>
2026-03-23 20:04:05 +0530

    no8

--- basic-problems/no8.java
+++ basic-problems/no8.java
@@ -1,15 +1,28 @@
 
-import java.util.Arrays;
+
+
 
 
 class no8 {
 
     public static void main(String args[]) {
-        //arrays basic
-        
-        int[] numbers = {10, 20, 30, 40};
-        System.out.println(Arrays.toString(numbers));
-
-        //[10, 20, 30, 40]
+        //Amstrogam number
+        int num=153;
+        int count=count(num);
+        int temp=num;
+        int ans=0;
+        while(num!=0){
+            int last=num%10;
+            ans=ans+(int)Math.pow(last,count);
+            num/=10;
+        }
+        System.out.println(ans);
+    }
+    public static  int count(int num){
+        int count=0;
+        for(int i=num;i>=0;i=i/10){
+            count++;
+        }
+        return(count);
     }
 }
