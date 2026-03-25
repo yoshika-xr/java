36a54772a8138a8ec4a5b9f0b4dd3fc1f0108599
yoshika-xr <yoshikajune@gmail.com>
2026-03-25 23:03:53 +0530

    no8

--- basic-problems/no8.java
+++ basic-problems/no8.java
@@ -1,24 +1,26 @@
-public class no8{
+
+public class no8 {
+
     public static void main(String[] args) {
-        int n=153;
-        int temp=n;
-        int sum=0;
-        int count=find_count(n);
-        while(temp!=0)
-        {
-          int rem=temp%10;
-          sum=sum+(int)Math.pow(rem,count);
-          temp=temp/10;
+        //found armstrong number//
+        int n = 153;
+        int temp = n;
+        int sum = 0;
+        int count = find_count(n);
+        while (temp != 0) {
+            int rem = temp % 10;
+            sum = sum + (int) Math.pow(rem, count);
+            temp = temp / 10;
         }
-        System.out.println((sum==n)?"Armstrong":"Not an Armstrong");
+        System.out.println((sum == n) ? "Armstrong" : "Not an Armstrong");
     }
-    public static int find_count(int n)
-    {
-        int count=0;
-        for(int i=n;i>0;i=i/10)
-        {
+
+    public static int find_count(int n) {
+        int count = 0;
+        for (int i = n; i > 0; i = i / 10) {
             count++;
         }
         return count;
     }
+    //Armstrong
 }
