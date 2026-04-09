f6b3201613ab0aa7a20024f19527569ec38ed45c
yoshika-xr <yoshikajune@gmail.com>
2026-04-09 18:51:55 +0530

    addfiles

---
+++ javacollec.java
@@ -0,0 +1,23 @@
+//custom class
+class Data {
+
+    public Integer num;
+    public String name;
+
+    Data(int _num, String _name) {
+        this.num = _num;
+        this.name = _name;
+    }
+}
+
+public class javacollec {
+
+    public static void main(String[] args) {
+        Data obj1 = new Data(9,"moha");
+        Data obj2 = new Data( 1,"maya");
+        obj1.name = "john";
+        System.out.println(obj2.name);
+    }
+}
+///john
+
