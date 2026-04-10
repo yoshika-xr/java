c4139b64636918cf3bb688916f71feb7a69e30d3
yoshika-xr <yoshikajune@gmail.com>
2026-04-10 22:13:48 +0530

    no2

---
+++ javacollection/no2.java
@@ -0,0 +1,17 @@
+
+class Data {
+
+    private Integer num;
+    private String name;
+
+    Data(int _num, String _name) {
+        this.num = _num;
+        this.name = _name;
+    }
+}
+public class  no2{
+    public static void main(String[] args) {
+        Data obj1=new Data(1,"maya");
+        Data obj2=new Data(2,"john");
+    }
+}
