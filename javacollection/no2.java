ce8ec6c947280c5a78a7e7f6ac43a226ce633293
yoshika-xr <yoshikajune@gmail.com>
2026-04-11 22:33:38 +0530

    no2

--- javacollection/no2.java
+++ javacollection/no2.java
@@ -1,4 +1,4 @@
-
+//get and set methods
 class Data {
 
     private Integer num;
@@ -8,10 +8,24 @@ class Data {
         this.num = _num;
         this.name = _name;
     }
+    public void setnum(int _num){
+        this.num=_num;
+    }
+    public void setname(String _name){
+        this.name=_name;
+    }
+    public  int getnum(){
+        return(num);
+    }
+    public String getname(){
+        return(name);
+    }
+
 }
 public class  no2{
     public static void main(String[] args) {
         Data obj1=new Data(1,"maya");
         Data obj2=new Data(2,"john");
+        System.out.println(obj1.getnum());
     }
 }
