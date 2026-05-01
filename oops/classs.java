51b95ffd8a48230db53a2248b87b6a33846a182f
yoshika-xr <yoshikajune@gmail.com>
2026-05-01 08:19:23 +0530

    class.oops

---
+++ oops/classs.java
@@ -0,0 +1,11 @@
+public class classs{
+    String city="London";
+    int population=9000000;
+
+    public static void main(String[] args) {
+        classs obj=new classs();
+        System.out.println("City:"+obj.city);
+        System.out.println("Population:"+obj.population);
+
+    }
+}
