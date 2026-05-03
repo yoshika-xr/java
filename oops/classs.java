2017a712fa0f0a90a0b058804bb41eacdef3ae86
yoshika-xr <yoshikajune@gmail.com>
2026-05-03 20:54:06 +0530

    class.oops

--- oops/classs.java
+++ oops/classs.java
@@ -1,11 +1,20 @@
+class opration{
+    int a=10;
+    int b=20;
+
+}
+
 public class classs{
     String city="London";
     int population=9000000;
 
     public static void main(String[] args) {
         classs obj=new classs();
+        opration obj1=new opration();
         System.out.println("City:"+obj.city);
         System.out.println("Population:"+obj.population);
+        System.out.println("Value of a:"+obj1.a);
+        System.out.println("Value of b:"+obj1.b);
 
     }
 }
