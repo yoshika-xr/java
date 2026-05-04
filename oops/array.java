47f32e9d214cd7942a34c5d9d0a1c9e86a0b3b63
yoshika-xr <yoshikajune@gmail.com>
2026-05-04 08:10:08 +0530

    array.oops

---
+++ oops/array.java
@@ -0,0 +1,14 @@
+public class array{
+    
+    public static void main(String[] args){
+    int nums[]={2,3,4,5};
+    System.out.println(nums[0]);//print one data using index
+
+    nums[2]=9;//change tha data using  index
+    System.out.println(nums[2]);
+    for(int i=0;i<4;i++){
+        System.out.println(nums[i]);
+    }
+
+
+    }}
