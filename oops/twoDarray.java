a2ba73d99f51deaa25dc091c3276376eb15f7ef9
yoshika-xr <yoshikajune@gmail.com>
2026-05-07 07:15:44 +0530

    2Darray.oops

---
+++ oops/twoDarray.java
@@ -0,0 +1,15 @@
+public  class twoDarray{
+    public static  void main(String[] args){
+        int nums[][]=new int[3][4];
+
+        //print the 2D array
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(nums[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
