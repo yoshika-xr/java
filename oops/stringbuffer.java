34a2e8c5867426e30836065e37248ef74d4e9665
yoshika-xr <yoshikajune@gmail.com>
2026-05-08 07:53:45 +0530

    stringbuffer

---
+++ oops/stringbuffer.java
@@ -0,0 +1,8 @@
+public class stringbuffer{
+    public static void main(String[] args) {
+        StringBuffer str=new StringBuffer("yash");
+        System.out.println(str.append("kira"));
+        // System.out.println(str.reverse());
+        System.out.println(str.delete(2, 5));
+    }
+}
