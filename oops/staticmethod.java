00d84cb8628952246d080d52454678c428cba12a
yoshika-xr <yoshikajune@gmail.com>
2026-05-12 08:04:01 +0530

    staticmethods.oops

--- oops/staticmethod.java
+++ oops/staticmethod.java
@@ -1,21 +1,29 @@
-class demo{
+
+class demo {
+
     String name;
     int cost;
     static String brand;
 
-    public demo(){
-        System.out.println(name+":"+cost+":"+brand);
+    public demo() {
+        System.out.println("it is constructor");
     }
-    public static void show(){
-        System.out.println(name+":"+cost+":"+brand);
+
+    public static void show(demo obj) {
+        System.out.println(obj.name + ":" + obj.cost + ":" + brand);
     }
 }
-public class staticmethod{
+
+public class staticmethod {
+
     public static void main(String[] args) {
-        demo obj=new demo();
-        obj.name="yash";
-        obj.cost=1000;  
-        obj.brand="phone";
-        
+        demo obj = new demo();
+        obj.name = "yash";
+        obj.cost = 1000;
+        obj.brand = "phone";
+
+        // obj.demo();
+        obj.show(obj);
+
     }
 }
