ff2ae5f4ef38a508566dc0493d0c754cd864507a
yoshika-xr <yoshikajune@gmail.com>
2026-05-20 20:18:07 +0530

    default and parameters constructor.oops

--- oops/constructor.java
+++ oops/constructor.java
@@ -10,6 +10,13 @@ class student {
         name = "yash";
     }
 
+
+//parameters constructor
+    public student(int a, String b) {
+        regno = a;
+        name = b;
+    }
+
     public void show() {
         System.out.println(regno + ":" + name);
 
@@ -22,5 +29,7 @@ public class constructor {
     public static void main(String[] args) {
         student obj = new student();
         obj.show();
+        student obj1 = new student(01, "maha");
+        obj1.show();
     }
 }
