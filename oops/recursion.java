a80764e12b54a0e32109bcffc77801fe3b863557
yoshika-xr <yoshikajune@gmail.com>
2026-07-05 18:07:31 +0530

    recursion.dsa

---
+++ oops/recursion.java
@@ -0,0 +1,20 @@
+
+public class recursion {
+
+    int count = 0;
+
+    public void name() {
+        if (count == 4) {
+            return;
+        }
+        System.out.println("maya");
+        count++;
+        name();
+
+    }
+
+    public static void main(String[] args) {
+        recursion r = new recursion();
+        r.name();
+    }
+}
