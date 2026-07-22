cd713b3df3710bb6d983d401558a59ac2e4cb716
yoshika-xr <yoshikajune@gmail.com>
2026-07-22 08:11:59 +0530

    hashing

--- oops/hashing.java
+++ oops/hashing.java
@@ -1,10 +1,13 @@
-public class hashing{
-    public static void main(String[] args){
-        int[] arr={2,3,4,5,6,7,7,8,8};
-        int[] hash=new int[10];
-        for(int i=0;i<arr.length;i++){
-           hash[ arr[i] ]++;
-        System.out.println(arr[i]+"->"+hash[arr[i]]);
+
+public class hashing {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 6, 7, 7, 8, 8};
+        int[] hash = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            hash[arr[i]]++;
+            System.out.println(arr[i] + "->" + hash[arr[i]]);
+            hash[10] = 0;
         }
     }
 }
